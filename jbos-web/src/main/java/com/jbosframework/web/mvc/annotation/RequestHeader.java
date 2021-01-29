990157233de543ae409e7a9cea9aea04b11f90cb
youfu.wang <youfu.wang@gmail.com>
2021-01-29 14:55:53 +0800

    提交代码

--- jbos-web/src/main/java/com/jbosframework/web/mvc/annotation/RequestHeader.java
+++
@@ -1,9 +0,0 @@
-package com.jbosframework.web.mvc.annotation;
-
-import java.lang.annotation.*;
-
-@Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.PARAMETER})
-public @interface RequestHeader {
-}
