f22f54ca83c23b4dba18241c6cb94cfd0eeaaccf
wangyufu19 <youfu.wang@gmail.com>
2023-01-16 16:05:58 +0800

    提交代码

--- jbos-core/src/main/java/com/jbosframework/core/annotaion/AnnotationAwareOrderComparator.java
+++ jbos-core/src/main/java/com/jbosframework/core/annotaion/AnnotationAwareOrderComparator.java
@@ -1,11 +1,33 @@
 package com.jbosframework.core.annotaion;
 
 import com.jbosframework.core.OrderComparator;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
 public class AnnotationAwareOrderComparator extends OrderComparator {
+    public static final AnnotationAwareOrderComparator INSTANCE = new AnnotationAwareOrderComparator();
 
+    public AnnotationAwareOrderComparator() {
+    }
+
+    protected Integer findOrder(Object obj) {
+        Integer order = super.findOrder(obj);
+        if (order != null) {
+            return order;
+        } else if (obj instanceof Class) {
+            return OrderUtils.getOrder((Class)obj);
+        } else {
+            Order ann;
+            if (obj instanceof Method) {
+                ann = (Order)AnnotationUtils.findAnnotation((Method)obj, Order.class);
+                if (ann != null) {
+                    return ann.value();
+                }
+            }
+            return order;
+        }
+    }
     public static void sort(List<?> list) {
         if (list.size() > 1) {
             list.sort(INSTANCE);
--- jbos-core/src/main/java/com/jbosframework/core/annotaion/AnnotationUtils.java
+++ jbos-core/src/main/java/com/jbosframework/core/annotaion/AnnotationUtils.java
@@ -3,10 +3,14 @@ package com.jbosframework.core.annotaion;
 import com.jbosframework.core.Nullable;
 import com.jbosframework.utils.Assert;
 import com.jbosframework.utils.ConcurrentReferenceHashMap;
+import com.jbosframework.utils.ReflectionUtils;
+import com.jbosframework.utils.StringUtils;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -14,6 +18,30 @@ import java.util.Set;
 public class AnnotationUtils {
     private static final Map<AnnotatedElement, Annotation[]> declaredAnnotationsCache = new ConcurrentReferenceHashMap(256);
 
+    @Nullable
+    public static Object getValue(Annotation annotation) {
+        return getValue(annotation, "value");
+    }
+
+    @Nullable
+    public static Object getValue(@Nullable Annotation annotation, @Nullable String attributeName) {
+        if (annotation != null && StringUtils.hasText(attributeName)) {
+            try {
+                Method method = annotation.annotationType().getDeclaredMethod(attributeName);
+                ReflectionUtils.makeAccessible(method);
+                return method.invoke(annotation);
+            } catch (NoSuchMethodException var3) {
+                return null;
+            } catch (InvocationTargetException var4) {
+                throw new IllegalStateException("Could not obtain value for annotation attribute '" + attributeName + "' in " + annotation, var4);
+            } catch (Throwable var5) {
+                var5.printStackTrace();
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
     public static <A extends Annotation> boolean isComponent(Class<?> type,@Nullable Class<A> annotationType) {
         if (AnnotationUtils.findAnnotation(type, annotationType) != null) {
             return true;
@@ -106,5 +134,12 @@ public class AnnotationUtils {
     public static boolean isInJavaLangAnnotationPackage(@Nullable String annotationType) {
         return annotationType != null && annotationType.startsWith("java.lang.annotation");
     }
-
+    public static <A extends Annotation> A findAnnotation(Method method, @Nullable Class<A> annotationType) {
+        Assert.notNull(method, "Method must not be null");
+        if (annotationType == null) {
+            return null;
+        } else {
+            return method.getDeclaredAnnotation(annotationType);
+        }
+    }
 }
